bb272583144a7e0083cedc41dadac46bc1cc8104
nothingrealm <shafaghi1378@gmail.com>
2020-04-13 19:28:29 +0430

    connecting main to db

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/nothingrealm/greenhouse/server/pkg/api"
+	"github.com/nothingrealm/greenhouse/server/pkg/db"
 )
 
 func main() {
-	e := echo.New()
+	_, err := db.Init()
 
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+
+	// echo initialization
+	e := echo.New()
 	e.Use(middleware.Logger())
 
+	// Router initialization
 	v1 := e.Group("/v1")
-
 	h := api.NewHandler()
 	h.Register(v1)
 
