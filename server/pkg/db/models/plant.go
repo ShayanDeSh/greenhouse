4c01c475e54faf0b58202186d0ca37a6d1cf2bc3
nothingrealm <shafaghi1378@gmail.com>
2020-04-14 01:18:07 +0430

    Working with Plant

--- server/pkg/db/models/plant.go
+++ server/pkg/db/models/plant.go
@@ -6,7 +6,28 @@ import (
 
 type Plant struct {
 	gorm.Model
-	Ip        string `gorm:"size:15;unique;not null"`
-	Humidity  int
-	NeedWater bool
+	Ip         string `gorm:"size:15;unique;not null"`
+	Humidity   int
+	NeedWater  bool
+	Threshhold int
+}
+
+func AddPlant(threshhold int, needwater bool, humidity int, ip string, db *gorm.DB) {
+	var plant = Plant{
+		Ip:         ip,
+		Humidity:   humidity,
+		NeedWater:  needwater,
+		Threshhold: threshhold,
+	}
+	db.Create(&plant)
+}
+
+func DeletePlant(ID int, db *gorm.DB) (err error) {
+	err = db.Where("id = ?", ID).Delete(&Plant{}).Error
+	return err
+}
+
+func UpdatePlant(plant Plant, db *gorm.DB) error {
+	err := db.Save(&plant).Error
+	return err
 }
