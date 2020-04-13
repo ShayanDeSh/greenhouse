971fbd300f62a63dfbe9c7f1518413e4dd039d92
nothingrealm <shafaghi1378@gmail.com>
2020-04-13 19:28:54 +0430

    Initializing db

---
+++ server/pkg/db/main.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/nothingrealm/greenhouse/server/pkg/db/models"
+)
+
+func Init() (db *gorm.DB, err error) {
+	db, err = gorm.Open("postgres",
+		"host=localhost port=5432 user=postgres dbname=greenhouse password=mypassword")
+	migrate(db)
+	return
+}
+
+func migrate(db *gorm.DB) {
+	db.AutoMigrate(&models.Plant{})
+}
