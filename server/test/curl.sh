df6511197c74733d2a49756908536fdb330844de
nothingrealm <shafaghi1378@gmail.com>
2020-04-14 19:02:32 +0430

    Test for testing the database

--- server/test/curl.sh
+++ server/test/curl.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-curl -X POST -H 'Content-Type:application/json' -d '{"Ip":"192.168.2.2","Humidity":23, "NeedWater":false, "Threshhold": 25}' http://localhost:8000/v1/plants/add
-
+curl -X POST -H 'Content-Type:application/json' -d '{"Ip":"192.168.2.6","Humidity":2, "NeedWater":false, "Threshhold": 25}' http://localhost:8000/v1/plants/add
